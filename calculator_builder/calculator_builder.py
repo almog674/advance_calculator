af9803b657f214d0c396fd2f7f55b5818ca11cbc
almog <almogmaimon674@gmail.com>
2022-10-09 19:55:47 +0300

    Add the class "CalculatorBuilder".

---
+++ calculator_builder/calculator_builder.py
@@ -0,0 +1,33 @@
+from calculator import Calculator
+from number_formatter.build_pipline_formatter import BuildPiplineFormatter
+from operators_manager.custom_operator import Operator
+from operators_manager.operators_manager import DictOperationsManager
+from parenthesis_handler.parenthesis_handler import ParenthesisHandler
+from parenthesis_handler.parenthesis_validator import ParenthesisValidator
+from statements_solver import StatementsSolver
+from sunequation_finder import SubequationFinder
+from utils.constants import SPECIAL_OPERATORS
+from utils.math_operations import get_min, get_mean, factorial, multiply, addition, subtraction, divide, power, modulu, \
+    get_max
+
+
+class CalculatorBuilder:
+    def __init__(self):
+        pass
+
+    def build_calculator(self) -> Calculator:
+        operators = {'&': Operator("&", get_min, 5, "binary"), '$': Operator("$", get_max, 5, "binary"),
+                     '@': Operator("@", get_mean, 5, "binary"), '%': Operator("%", modulu, 4, "binary"),
+                     '!': Operator("!", factorial, 4, "unery"), '^': Operator("^", power, 3, "binary"),
+                     '*': Operator("*", multiply, 2, "binary"), '/': Operator("/", divide, 2, "binary"),
+                     '+': Operator("+", addition, 1, "binary"), '-': Operator("-", subtraction, 1, "binary")}
+
+        operations_manager = DictOperationsManager(operators)
+        numbers_formatter_builder = BuildPiplineFormatter()
+        numbers_formatter = numbers_formatter_builder.build_pipline_formatter()
+
+        parenthesis_handler = ParenthesisHandler(ParenthesisValidator())
+        subequation_finder = SubequationFinder(operations_manager, parenthesis_handler, SPECIAL_OPERATORS)
+        statements_solver = StatementsSolver(subequation_finder, numbers_formatter)
+
+        return Calculator(statements_solver)
